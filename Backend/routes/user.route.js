e72c2e4215fdbf90422a8ebf79e425fdb2c20c8a
laxmigupta19 <laxmigupta191003@gmail.com>
2026-03-12 23:58:24 +0530

    fix

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express";import { logout } from "../controller/user.controller.js";
 import {
   allUsers,
   login,
--- Frontend/src/context/useGetAllUsers.jsx
+++ Frontend/src/context/useGetAllUsers.jsx
@@ -10,7 +10,7 @@ function useGetAllUsers() {
       try {
         const token = Cookies.get("jwt");
         const response = await axios.get("https://chatapplication-kbtr.onrender.com/api/user/allusers", {
-          withCredentialscredentials: true,
+          withCredentials: true,
           headers: {
             Authorization: `Bearer ${token}`,
           },

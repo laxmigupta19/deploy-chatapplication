5b3bcbde0622fad0d183410105ae8e8b5d3b635d
laxmigupta19 <laxmigupta191003@gmail.com>
2026-03-12 23:51:21 +0530

    fix

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -9,6 +9,7 @@ const io = new Server(server, {
   cors: {
     origin: "https://deploy-chatapplication.vercel.app",//origin: "http://localhost:3001",
     methods: ["GET", "POST"],
+    credentials:true,
   },
 });
 
--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -59,8 +59,12 @@ export const login = async (req, res) => {
 };
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("jwt");
-    res.status(201).json({ message: "User logged out successfully" });
+    res.clearCookie("jwt",{
+      httpOnly:true,
+      secure:true,
+      sameSite:"none",
+    });
+    res.status(200).json({ message: "User logged out successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal server error" });
